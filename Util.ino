6e0795b653cb16f741452d80cee154007d3c56b6
Graziella Sundblad <GrazySE@users.noreply.github.com>
2015-05-19 11:37:08 +0200

    Create Util.ino

---
+++ Util.ino
@@ -0,0 +1,24 @@
+void resetLEDS() {
+for (int thisPin = 0; thisPin < 8; thisPin++) {
+digitalWrite(row[thisPin], HIGH);     // Set all cathodes to have positive voltage at them. Hence LED will not light up irrespective of what the anode is set to
+ digitalWrite(col[thisPin], LOW); 
+
+ }
+} 
+  
+ void onLED(int colnum,int rownum) {
+  digitalWrite(col[colnum], HIGH);
+  digitalWrite(row[rownum], LOW);
+delay(delayPin);
+digitalWrite(col[colnum], LOW);
+digitalWrite(row[rownum], HIGH);
+}
+
+void setPins()
+{
+   for (int thisPin = 0; thisPin < 8; thisPin++) {
+    pinMode(col[thisPin], OUTPUT);      // Set selected pin to be an output rather then an input pin
+    pinMode(row[thisPin], OUTPUT);       // this is so we cna provide +5v or GND at any pin
+    digitalWrite(row[thisPin], HIGH);     // Set all cathodes to have positive voltage at them. Hence LED will not light up irrespective of what the anode is set to
+  }
+}

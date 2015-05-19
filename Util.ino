cae2851c002c8ce745f76be36d2c133d54d2191f
Graziella Sundblad <GrazySE@users.noreply.github.com>
2015-05-19 13:51:44 +0200

    Update Util.ino

--- Util.ino
+++ Util.ino
@@ -1,7 +1,7 @@
 void resetLEDS() {
-for (int thisPin = 0; thisPin < 8; thisPin++) {
-digitalWrite(row[thisPin], HIGH);     // Set all cathodes to have positive voltage at them. Hence LED will not light up irrespective of what the anode is set to
- digitalWrite(col[thisPin], LOW); 
+ for (int thisPin = 0; thisPin < 8; thisPin++) {
+  digitalWrite(row[thisPin], HIGH);     // Set all cathodes to have positive voltage at them. Hence LED will not light up irrespective of what the anode is set to
+  digitalWrite(col[thisPin], LOW); 
 
  }
 } 
@@ -9,16 +9,16 @@ digitalWrite(row[thisPin], HIGH);     // Set all cathodes to have positive volta
  void onLED(int colnum,int rownum) {
   digitalWrite(col[colnum], HIGH);
   digitalWrite(row[rownum], LOW);
-delay(delayPin);
-digitalWrite(col[colnum], LOW);
-digitalWrite(row[rownum], HIGH);
+  delay(delayPin);
+  digitalWrite(col[colnum], LOW);
+  digitalWrite(row[rownum], HIGH);
 }
 
 void setPins()
 {
    for (int thisPin = 0; thisPin < 8; thisPin++) {
-    pinMode(col[thisPin], OUTPUT);      // Set selected pin to be an output rather then an input pin
-    pinMode(row[thisPin], OUTPUT);       // this is so we cna provide +5v or GND at any pin
-    digitalWrite(row[thisPin], HIGH);     // Set all cathodes to have positive voltage at them. Hence LED will not light up irrespective of what the anode is set to
-  }
+     pinMode(col[thisPin], OUTPUT);      // Set selected pin to be an output rather then an input pin
+     pinMode(row[thisPin], OUTPUT);       // this is so we cna provide +5v or GND at any pin
+     digitalWrite(row[thisPin], HIGH);     // Set all cathodes to have positive voltage at them. Hence LED will not light up irrespective of what the anode is set to
+   }
 }

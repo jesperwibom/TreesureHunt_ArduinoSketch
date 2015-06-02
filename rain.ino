4321cfac9146f6d604b5629686e99e934fbb4be0
Graziella Sundblad <GrazySE@users.noreply.github.com>
2015-06-02 14:28:30 +0200

    Smart code

--- rain.ino
+++ rain.ino
@@ -2,174 +2,253 @@ void rain1()
 {
  
  
- onLED(0,0);
- onLED(0,2);
- onLED(0,5);
- onLED(0,7);
- onLED(1,0);
- onLED(1,2);
- onLED(1,5);
- onLED(1,7);
+  for (int R=0; R<2; R++)
+  {
+    onLED(R,0);
+    onLED(R,2);
+    onLED(R,5);
+    onLED(R,7);
+  
 }
-
+}
 void rain2()
 {
-  
-  
- onLED(0,1);
- onLED(0,4);
- onLED(1,0);
- onLED(1,2);
- onLED(1,5);
- onLED(1,7);
- onLED(2,0);
- onLED(2,2);
- onLED(2,5);
- onLED(2,7);
-
+  for (int R=1; R<3; R++)
+  {
+    onLED(R,0);
+    onLED(R,2);
+    onLED(R,5);
+    onLED(R,7);
+  }
+  for (int C=1; C<7; C=C+3)
+  {
+onLED(0,C);
+  }
 }
 
 void rain3()
 {
 
+  
+   for (int C=1; C<7; C=C+3)
+  {
+onLED(0,C);
+onLED(1,C);
+  }
 
- onLED(0,1);
- onLED(0,4);
- onLED(1,1);
- onLED(1,4);
- onLED(2,0);
- onLED(2,2);
- onLED(2,5);
- onLED(2,7);
- onLED(3,0);
- onLED(3,2);
- onLED(3,5);
- onLED(3,7);
+ for (int R=2; R<4;R++)
+ {
+   onLED(R,0);
+   onLED(R,2);
+   onLED(R,5);
+   onLED(R,7);
+ }
+
 
 }
 
 void rain4()
 {
 
+  
+  for (int C=1; C<7; C=C+3)
+  {
+    onLED(1,C);
+    onLED(2,C);
+  
+  }
+
+ for (int R=3; R<5;R++)
+ {
+
+onLED(R,0);
+onLED(R,2);
+onLED(R,5);
+onLED(R,7);
+ }
+ 
  onLED(0,6);
- onLED(1,1);
- onLED(1,4);
- onLED(2,1);
- onLED(2,4);
- onLED(3,0);
- onLED(3,2);
- onLED(3,5);
- onLED(3,7);
- onLED(4,0);
- onLED(4,2);
- onLED(4,5);
- onLED(4,7);
 
 }
+
+
 
 void rain5()
 {
   
-  
- onLED(0,3);
- onLED(0,6);
- onLED(1,1);
- onLED(1,6);
- onLED(2,1); 
- onLED(2,4);
- onLED(3,4);
- onLED(4,0);
- onLED(4,2);
- onLED(4,5);
- onLED(4,7);
- onLED(5,0);
- onLED(5,2);
- onLED(5,5);
- onLED(5,7);
 
+  for (int R=0;R<2;R++)
+  {
+  
+
+    onLED(R,6);
+  }
+
+  for (int R=1; R<3; R++)
+  {
+    onLED(R,1);
+  }
+
+  for (int R=2; R<4; R++)
+  {
+    onLED(R,4);
+  }
+
+ for (int R=4; R<6;R++)
+ {
+
+    onLED(R,0);
+    onLED(R,2);
+    onLED(R,5);
+    onLED(R,7);
+  }
+
+onLED(0,3);
 }
 
 
 void rain6()
 {
   
- onLED(0,0);
- onLED(0,3);
- onLED(1,3);
- onLED(1,6);
- onLED(2,1);
- onLED(2,6);
- onLED(3,1);
- onLED(3,4);
- onLED(4,4);
- onLED(5,0);
- onLED(5,5);
- onLED(5,7);
- onLED(6,0);
- onLED(6,2);
- onLED(6,5);
- onLED(6,7);
- onLED(7,0);
+
+  for (int R=1; R<3; R++){
+  onLED (R,6);
+  }
+
+
+
+  for (int R=2; R<4;R++){
+    onLED(R,1);
+    }
+
+
+  for (int R=0; R<2;R++)
+  {
+      onLED(R,3);
+  }
+
+  for (int R=5; R<8; R++)
+  {
+  
+  onLED(R,0);
+  }
+
+
+  for (int R=3; R<5;R++)
+  {
+
+    onLED(R,4);
+  }
+
+
+  for (int C=5; C<8; C=C+2)
+  {
+  onLED(5,C);
+  onLED(6,C);
+  }
+
+
+onLED(0,0);
+onLED(6,2);
 
 }
-
-
 
 void rain7()
 {
   
  
- onLED(0,0);
- onLED(0,5);
- onLED(1,0);
- onLED(1,3);
- onLED(2,3); 
- onLED(2,6);
- onLED(3,1);
- onLED(3,6);
- onLED(4,1);
- onLED(4,4);
- onLED(5,4);
- onLED(6,0);
- onLED(6,2);
- onLED(6,5);
- onLED(6,7);
- onLED(7,0);
- onLED(7,2);
- onLED(7,5);
- onLED(7,7);
+  
+  for (int R=0; R<2; R++){
+    
+    onLED(R,0);
+    }
+
+  for (int R=1; R<3; R++){
+    
+    onLED(R,3);
+    }
+
+  for (int R=2; R<4; R++){
+    
+    onLED(R,6);
+    }
+
+  for (int R=3; R<5; R++){
+    
+    onLED(R,1);
+    }
+
+
+
+  for (int R=4; R<6; R++){
+    
+    onLED(R,4);
+    }
+
+  for (int R=6; R<8;R++){
+    
+    onLED(R,0);
+    onLED(R,2);
+    onLED(R,5);
+    onLED(R,7);
+
+  }
+  
+  
+  
+  onLED(0,5);
 
 }
-
-
 
 void rain8()
 {
   
   
+  for (int R=2; R<4;R++){
+    for (int C=3;C<7;C=C+3){
+    onLED(R,C);
+    }
+  }
   
- onLED(0,5);
- onLED(1,0);
- onLED(1,5);
- onLED(2,0);
- onLED(2,3);
-   
- onLED(2,6);
- onLED(3,3);
- onLED(3,6);
- onLED(4,1);
- onLED(5,1);
-   
- onLED(5,4);
- onLED(6,4);
- onLED(7,0);
- onLED(7,2);
- onLED(7,5);
- onLED(7,7);
+  
+  for (int R=0; R<2; R++)
+  {
+    onLED(R,5);
+  }
+
+
+
+ for (int R=1; R<3; R++)
+  {
+    onLED(R,0);
+  }
+
+
+ for (int R=4; R<6; R++)
+  {
+    onLED(R,1);
+  }
+
+ for (int R=51; R<7; R++)
+  {
+    onLED(R,4);
+  }
+
+
+  for (int C=0;C<3; C=C+2){
+  
+  onLED(7,C);
+  }
+
+
+   for (int C=5;C<8; C=C+2){
+
+    onLED(7,C);
+  
+  
+    }
 
 }
-
-
 
 void rainANI (){
  
@@ -180,48 +259,45 @@ void rainANI (){
   }
   
   for (int x=0;x<20;x++) {
-  rain2();
+ rain2();
   delay(1);
     } 
   
    for (int x=0;x<20;x++) {
-   rain3();
-   delay(1);
+ rain3();
+  delay(1);
     } 
   
    for (int x=0;x<20;x++) {
-    rain4();
-    delay(1);
+  rain4();
+  delay(1);
     } 
     
     
     
   for (int x=0;x<20;x++) {
-   rain5();
-   delay(1);
+  rain5();
+  delay(1);
     }    
  
     
   for (int x=0;x<20;x++) {
-   rain6();
-   delay(1);
+  rain6();
+  delay(1);
     }  
  
  
     
   for (int x=0;x<20;x++) {
-   rain7();
-   delay(1);
+  rain7();
+  delay(1);
     }  
  
     
   for (int x=0;x<20;x++) {
-   rain8();
-   delay(1);
+  rain8();
+  delay(1);
     }  
  
  
 }
-
-
-
